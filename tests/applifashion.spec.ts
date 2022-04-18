2c08bd3f9bba1bb2df1797bd9abfe9f7c5e3cbdd
Pandy Knight <andylpk.247@gmail.com>
2022-04-18 14:19:38 -0400

    Added Playwright tests

---
+++ tests/applifashion.spec.ts
@@ -0,0 +1,61 @@
+import { test } from '@playwright/test';
+import { VisualGridRunner, Eyes, Configuration, BatchInfo, BrowserType, Target } from '@applitools/eyes-playwright';
+
+test.describe.configure({ mode: 'parallel' })
+
+test.describe('AppliFashion', () => {
+  const URL_MAIN = 'https://demo.applitools.com/tlcHackathonMasterV1.html';
+  const URL_DEV = 'https://demo.applitools.com/tlchackathondev';
+  const URL_PROD = 'https://demo.applitools.com/tlcHackathonMasterV2.html';
+
+  let eyes: Eyes;
+  let runner: VisualGridRunner;
+  
+  test.beforeAll(async () => {
+    const configuration = new Configuration();
+    configuration.setBatch(new BatchInfo({name: 'AppliFashion Tests', id: 'applifashion'}));
+    configuration.addBrowser(1200, 800, BrowserType.CHROME);
+
+    runner = new VisualGridRunner({ testConcurrency: 5 });
+    eyes = new Eyes(runner);
+    eyes.setConfiguration(configuration);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await page.setViewportSize({width: 1600, height: 1200});
+    // await page.goto(URL_MAIN);
+    // await page.goto(URL_DEV);
+    await page.goto(URL_PROD);
+  });
+  
+  test('should load the main page', async ({ page }) => {
+    await eyes.open(page, 'AppliFashion', '1. Main Page');
+    await eyes.check('Main page', Target.window().fully());
+    await eyes.close(false);
+  });
+  
+  test('should filter by color', async ({ page }) => {
+    await eyes.open(page, 'AppliFashion', '2. Filtering');
+    await page.locator('id=SPAN__checkmark__107').click();
+    await page.locator('id=filterBtn').click();
+    await eyes.checkRegionBy('#product_grid', 'Filter by color')
+    await eyes.close(false);
+  });
+  
+  test('should ?', async ({ page }) => {
+    await eyes.open(page, 'AppliFashion', '3. Product Page');
+    await page.locator('text="Appli Air x Night"').click();
+    await page.locator('id=shoe_img').waitFor();
+    await eyes.check('Product details', Target.window().fully());
+    await eyes.close(false);
+  });
+
+  test.afterEach(async () => {
+    await eyes.abort();
+  });
+
+  test.afterAll(async() => {
+    const results = await runner.getAllTestResults(false);
+    console.log('Visual test results', results);
+  })
+});
